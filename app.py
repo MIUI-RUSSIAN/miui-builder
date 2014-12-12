70637a555f36016129b44690f2f6b16f52916d6c
eggfly <lihaohua90@gmail.com>
2014-12-12 20:06:33 +0800

    fix crash
    Signed-off-by: eggfly <lihaohua90@gmail.com>

--- app.py
+++ app.py
@@ -3,25 +3,25 @@
 
 import os.path
 
+import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         # self.write("Hello, world")
-        # projects = [{'repo': 'cancro-v6-kk-alpha'}]
-        # self.render("index.html", projects=projects)
-        self.render("index.html")
-
-application = tornado.web.Application(
-    handlers = [
-      (r"/", MainHandler),
-    ], 
-    template_path=os.path.join(os.path.dirname(__file__), "templates"),
-    static_path=os.path.join(os.path.dirname(__file__), "static"),
-)
+        projects = [{'repo': 'cancro-v6-kk-alpha'}]
+        self.render("index.html", projects=projects)
 
 if __name__ == "__main__":
-    application.listen(8080)
+    app = tornado.web.Application(
+        handlers = [
+            (r"/", MainHandler),
+        ],
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        static_path=os.path.join(os.path.dirname(__file__), "static"),
+    )
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(8080)
     tornado.ioloop.IOLoop.instance().start()
 
