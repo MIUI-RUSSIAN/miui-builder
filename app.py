95a9ff18d780d9975a433d2d24c3e4ce6af6c630
eggfly <lihaohua90@gmail.com>
2014-12-12 19:58:10 +0800

    test server
    Signed-off-by: eggfly <lihaohua90@gmail.com>

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os.path
+
 import tornado.ioloop
 import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        # self.write("Hello, world")
+        # projects = [{'repo': 'cancro-v6-kk-alpha'}]
+        # self.render("index.html", projects=projects)
+        self.render("index.html")
 
-application = tornado.web.Application([
-    (r"/", MainHandler),
-])
+application = tornado.web.Application(
+    handlers = [
+      (r"/", MainHandler),
+    ], 
+    template_path=os.path.join(os.path.dirname(__file__), "templates"),
+    static_path=os.path.join(os.path.dirname(__file__), "static"),
+)
 
 if __name__ == "__main__":
     application.listen(8080)
