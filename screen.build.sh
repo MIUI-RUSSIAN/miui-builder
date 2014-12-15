764e3669980b9b0d81ffe1d0828493cc282f6a63
eggfly <lihaohua90@gmail.com>
2014-12-16 01:40:40 +0800

    fix error
    Signed-off-by: eggfly <lihaohua90@gmail.com>

--- screen.build.sh
+++ screen.build.sh
@@ -1,5 +1,5 @@
 if [ `screen -list | grep "$2:build" -c` = 0 ]; then
-cd "$1" && screen -S "$2:sync" -d -m ~/miui-builder/build.sh
+cd "$1" && screen -S "$2:build" -d -m ~/miui-builder/build.sh
 else
 echo "already running build on $1"
 fi
--- screen.sync.sh
+++ screen.sync.sh
@@ -1,5 +1,5 @@
-if [ `screen -list | grep "$2:build" -c` = 0 ]; then
-cd "$1" && screen -S "$2:build" -d -m ~/miui-builder/sync.sh
+if [ `screen -list | grep "$2:sync" -c` = 0 ]; then
+cd "$1" && screen -S "$2:sync" -d -m ~/miui-builder/sync.sh
 else
 echo "already running sync on $1"
 fi
