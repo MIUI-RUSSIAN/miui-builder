56bc87d1ad558007c58d9246ebf39495b4356e30
eggfly <lihaohua90@gmail.com>
2015-02-04 21:26:52 +0800

    Added leo to product_path.py
    Signed-off-by: eggfly <lihaohua90@gmail.com>

--- product_path.py
+++ product_path.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 
-PRODUCTS = ["aries", "cancro", "pisces", "virgo", "mocha", "ferrari", "gucci"]
+PRODUCTS = ["aries", "cancro", "pisces", "virgo", "mocha", "ferrari", "gucci", "leo"]
 path = os.path.normpath(os.getcwd())
 parts = path.split(os.sep)
 target = []
